ba8c2edaa93f11fb29a54a7d6e99a0c1232db28e
MichaelC609 <mjcastillo@cpp.edu>
2026-06-07 10:44:12 -0700

    changed land handler to match takeoff handler logic

--- backend/simulation/handlers/land_handler.py
+++ backend/simulation/handlers/land_handler.py
@@ -4,26 +4,22 @@ from drone import commands
 
 def execute(command, state_manager, dt):
     #get current drone state
-    currentState = StateManager.getState()
+    currentState = state_manager.getState()
 
     #retrieve altitude
     altitude = currentState.position.z
 
     #retrieve target altitude and takeoff speed
     target_altitude = 0 #0m
-    landing_speed = commands.LandingPayload.landing_speed
+    landing_speed = command.landing.landing_speed
 
     #compute altitude change and clamp new altitude
     altitude_delta = landing_speed * dt
     new_altitude = max((altitude - altitude_delta), target_altitude)
 
     #update state manager
-    StateManager.updateState(new_altitude)
+    state_manager.updateState(new_altitude)
 
     #check for completion
-    if new_altitude == 0:
-        return True
-    
-    else:
-        return False
+    return new_altitude == target_altitude
 
